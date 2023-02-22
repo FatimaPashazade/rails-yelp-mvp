1604a93b19a8db02cfc32b146cc7104579669a5e
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-02-22 16:56:17 +0100

    Rails Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews
+  end
+  #resources :restaurants, only: [:index, :show, :new, :create] do
+    #resources :reviews, only: [:new, :create]
+  #end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #resources :reviews, only: [:destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create!(name: 'Noodles Queen', address: 'Pau Claris 50', category: 'chinese')
+puts 'Created restaurant: Pasta Queen'
+Restaurant.create!(name: 'Pizza Best', address: 'Main street 20', category: 'italian')
+puts 'Created restaurant: Pizza Best'
+Restaurant.create!(name: 'Sushi bowls', address: 'Carrer de Bruc', category: 'japanese')
+puts 'Created restaurant: Quinoa bowls'
+Restaurant.create!(name: 'Pastry Queen', address: 'Paris street 22', category: 'french')
+puts 'Created restaurant: Sushi place'
+Restaurant.create!(name: 'Choco Heaven', address: 'Left street 34', category: 'belgian')
+puts 'Created restaurant: Mexican'
